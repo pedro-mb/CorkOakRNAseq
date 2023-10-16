3980dbe99bc194a3d149736c75b25650733dc9ef
Pedro Barros <pedro.pataias@gmail.com>
2023-10-16 18:43:57 +0100

    Add files via upload

--- DE_workflow_final.R
+++ DE_workflow_final.R
@@ -1,16 +1,8 @@
----
-title: "R Notebook"
-output:
-  html_document:
-    df_print: paged
----
+# RNA-seq analysis of cork oak Stem samples from plants after 6mo of drought (WD water deficit .vs. WW well watered)
 
-RNA-seq analysis of cork oak Stem samples from plants after 6mo of drought (WD water deficit .vs. WW well watered)
+### load raw counts table
+#"RawReadCounts_stems2021.txt" available at ArrayExpress Accession: E-MTAB-13376
 
-## Load raw counts table
-"RawReadCounts_stems2021.txt" available at ArrayExpress Accession: E-MTAB-13376
-
-```{r}
 # Load main packages
 suppressMessages(library(DESeq2))
 library(ggplot2)
@@ -64,11 +56,8 @@ dds <- DESeqDataSetFromMatrix(countData=countdata, colData=coldata,
 dds
 
 
-```
-
 ### Convert raw counts in Transcript per million (for heatmaps)
 
-```{r}
 # counts_to_TPM.R available here:
 # https://rdrr.io/github/skimlab/CCSBUtils/src/R/counts_to_TPM.R 
 
@@ -100,11 +89,8 @@ gene_sd_tpm <- apply(meanExpression_tpm, 1, sd)
 all_zscore_tpm <- (meanExpression_tpm - gene_means_tpm)/gene_sd_tpm
 
 
-```
+### PCAs e clustering
 
-
-## PCAs e clustering
-```{r}
 # Principal Component Analysis (PCA) with ggplot2
 
 vsd <- vst(dds, blind = TRUE) 
@@ -118,7 +104,7 @@ pcaDATA <- plotPCA(vsd,ntop = 1000,intgroup = c("Treatment","Tissue"),
 percentvar <- round(100*attr(pcaDATA,"percentVar"))
 
 pca_plot <- ggplot(pcaDATA,aes(PC1,PC2,colour = Tissue, 
-                              shape = Treatment)) +
+                               shape = Treatment)) +
   geom_point(size = 4) +
   xlab(paste0("PC1: ",percentvar[1],"% variance")) +
   ylab(paste0("PC2: ",percentvar[2],"% variance")) +
@@ -141,20 +127,18 @@ colnames(sampleDistMatrix) <- colnames(vsd)
 
 colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
 hdist <- pheatmap(sampleDistMatrix,
-         clustering_distance_rows=sampleDists,
-         clustering_distance_cols=sampleDists,
-         col=colors)
+                  clustering_distance_rows=sampleDists,
+                  clustering_distance_cols=sampleDists,
+                  col=colors)
 
 hdist
 #ggsave(hdist, file = "RNAseq_Stems_dist.png", device = "png", width = 1800, height = 1400, units = "px", dpi = 300)
 #ggsave(hdist, file = "RNAseq_Stems_dist.svg", width = 1800, height = 1400, units = "px", device = "svg")
 
 
-```
 
-## A: Run multigroup comparison analysis (Likelihood ratio test, ANOVA-like)
+### A: Run multigroup comparison analysis (Likelihood ratio test, ANOVA-like)
 
-```{r}
 # get DEGs between tissues, i.e genes with differential expression in 
 # at least one of the following pair-wise comparisons:
 # Phellem vs Xylem; Phellem vs InnerBark; InnerBark vs Xylem
@@ -172,10 +156,9 @@ resSig <- subset(resSig, baseMean >= baseMean.cutoff)
 # Get number of significant genes
 nrow(resSig)
 
-```
 
 ### Calculate mean expression of biological replicates and z-score for plotting  
-```{r}
+
 # Extract Transformed Values / Regularize Logaritms 
 vsd <- vst(dds,blind = FALSE)
 
@@ -193,14 +176,13 @@ gene_sd_all <- apply(meanExpression, 1, sd)
 all_zscore <- (meanExpression - gene_means_all)/gene_sd_all
 
 coldata_mean <- data.frame(row.names = colnames(all_zscore),
-                      Treatment=c(rep(c("WW","WD"),3)),
-                      Tissue=c(rep("Phellem",2), rep("InnerBark",2),
-                               rep("Xylem", 2)))
-```
+                           Treatment=c(rep(c("WW","WD"),3)),
+                           Tissue=c(rep("Phellem",2), rep("InnerBark",2),
+                                    rep("Xylem", 2)))
 
 
 ### Clustering analysis (k-means)
-```{r, eval=FALSE}
+
 source("bin/clustering_functions.R")
 suppressMessages(library(tidyr))
 
@@ -221,7 +203,7 @@ meanExpression_DEGs$cluster_1 <- clusterExpr(meanExpression_DEGs[, 1:3], ncluste
 
 clusterDEGs_long <- meanExpression_DEGs[,c(1:5)] %>% 
   pivot_longer(c(Phellem:Xylem), names_to = "sample", values_to = "ncounts") #%>%
-  #mutate(ncounts = log2(ncounts) + 1)
+#mutate(ncounts = log2(ncounts) + 1)
 
 #### block the order of x axis if necessary
 levels1=c("Phellem", "InnerBark", "Xylem")
@@ -242,7 +224,7 @@ clust_1 <- ggplot(clusterDEGs_long, aes(sample, ncounts)) +
     axis.text.x = element_text(size=10),
     legend.text = element_text(size = 10),
     strip.background = element_rect(colour="black", fill=c("grey95","black"))
-        )
+  )
 
 #ggsave("DEGs_RNAseq_clust6_all.001.svg", plot = clust_1, width = 10, height = 6 )
 
@@ -259,43 +241,39 @@ clust_2 <- ggplot(clusterDEGs_long, aes(sample, ncounts, group = cluster_1)) +
     axis.text.x = element_text(size=12),
     legend.text = element_text(size = 14),
     strip.background = element_rect(colour="black", fill=c("grey95","black"))
-        )
+  )
 #ggsave("DEGs_RNAseq_clust6_0.001.svg", plot = clust_2, width = 10, height = 6 )
 
 #Table S3
 
 #write.csv(merge(resSig, meanExpression_DEGs[,c(4,5)], by =0), file = "DEGs_LRT_multitissue_kmeans.csv")
 
-```
-
-
 ### Draw heatmaps for candidate genes
-```{r}
 mylength <- 100
 
 cambiumMarkers <- c("LOC111988759", #WOX4 *
-"LOC112027031", #CLE44
-"LOC112012413", #EPFL4 *
-"LOC112024199", #EPFL6
-"LOC112019802", #PXY/TDR *
-"LOC112031035", #SHR *
-"LOC111993990", #STM.2 *
-"LOC112032307", #STM.1 *
-"LOC111997860", #ER  *
-"LOC112031780", #ER?
-"LOC111992965", #ERL1
-"LOC112038775", #LBD4 *
-"LOC112024503", #LBD11
-"LOC111997151", #LBD4
-"LOC111999314", #ARR5 *
-"LOC112030198", #AINTEGUMENTA *
-"LOC112015571", #KNAT/BP *
-"LOC112038380", #CYCD3;1
-"LOC112022222", #CYCD3;1 *
-"LOC111985236", #AINTEGUMENTA *
-"LOC112011953", #ARF5/MP *
-"LOC112026395", #TMO5
-"LOC111992348"#LHW  *
+                    "LOC112027031", #CLE44
+                    "LOC112012413", #EPFL4 *
+                    "LOC112024199", #EPFL6
+                    "LOC112019802", #PXY/TDR *
+                    "LOC112031035", #SHR *
+                    "LOC111993990", #STM.2 *
+                    "LOC112032307", #STM.1 *
+                    "LOC111997860", #ER  *
+                    "LOC112031780", #ER?
+                    "LOC111992965", #ERL1
+                    "LOC112038775", #LBD4 *
+                    "LOC112024503", #LBD11
+                    "LOC111997151", #LBD4
+                    "LOC111999314", #ARR5 *
+                    "LOC112030198", #AINTEGUMENTA *
+                    "LOC112015571", #KNAT/BP *
+                    "LOC112038380", #CYCD3;1
+                    "LOC112022222", #CYCD3;1 *
+                    "LOC111985236", #AINTEGUMENTA *
+                    "LOC112011953", #ARF5/MP *
+                    "LOC112026395", #TMO5
+                    "LOC111992348"#LHW  *
 )
 
 list <- all_zscore[rownames(all_zscore) %in% intersect(cambiumMarkers, rownames(resSig)),]
@@ -304,37 +282,37 @@ mybreaks<- c(seq(min(list), 0, length.out=ceiling(mylength/2) + 1), seq(max(list
 
 
 p <- pheatmap(list, 
-         cluster_rows=TRUE, 
-         show_rownames=TRUE,
-         cluster_cols=FALSE, 
-         color = colorRampPalette(rev(brewer.pal(n = 9, name = "RdBu")))(100),
-         annotation_col = coldata_mean,
-         breaks = mybreaks,
-         border_color = "white",
-         cellwidth = 20,        
-         cellheight = 10,
-         fontsize_row = 8,
-         annotation_colors = ann_colors)
+              cluster_rows=TRUE, 
+              show_rownames=TRUE,
+              cluster_cols=FALSE, 
+              color = colorRampPalette(rev(brewer.pal(n = 9, name = "RdBu")))(100),
+              annotation_col = coldata_mean,
+              breaks = mybreaks,
+              border_color = "white",
+              cellwidth = 20,        
+              cellheight = 10,
+              fontsize_row = 8,
+              annotation_colors = ann_colors)
 
 #ggsave("DEGsANOVA_CambiumMarkers.svg", plot = p, width = 6, height = 10.4 )
 
 
 # tissue differentiation markers  
 diffMarkers<- c("LOC112007487", #MYB84
-               "LOC112030452", #MYB93
-               "LOC111991558", #WRKY13
-               "LOC112038777", #MYB83
-               "LOC111996101", #KAN
-               "LOC112040698", #KAN
-               "LOC112010403", #ATHB15
-               "LOC112031480", #PHABOLOSA Athb14
-               "LOC112038274", #REVOLUTA
-               "LOC112001733", #asft1
-               "LOC112007140",#asft2
-               "LOC112033310", #MYB93
-               "LOC112028279", #GPAT5
-               "LOC111990215", #APL
-               "LOC112007669" #APL
+                "LOC112030452", #MYB93
+                "LOC111991558", #WRKY13
+                "LOC112038777", #MYB83
+                "LOC111996101", #KAN
+                "LOC112040698", #KAN
+                "LOC112010403", #ATHB15
+                "LOC112031480", #PHABOLOSA Athb14
+                "LOC112038274", #REVOLUTA
+                "LOC112001733", #asft1
+                "LOC112007140",#asft2
+                "LOC112033310", #MYB93
+                "LOC112028279", #GPAT5
+                "LOC111990215", #APL
+                "LOC112007669" #APL
 )
 
 
@@ -344,25 +322,23 @@ mybreaks<- c(seq(min(list), 0, length.out=ceiling(mylength/2) + 1), seq(max(list
 
 
 p<- pheatmap(list, 
-         cluster_rows=TRUE, 
-         show_rownames=TRUE,
-         cluster_cols=FALSE, 
-         color = colorRampPalette(rev(brewer.pal(n = 9, name = "RdBu")))(100),
-         annotation_col = coldata_mean,
-         breaks = mybreaks,
-         border_color = "white",
-         cellwidth = 20,        
-         cellheight = 10,
-         fontsize_row = 8,
-         annotation_colors = ann_colors)
+             cluster_rows=TRUE, 
+             show_rownames=TRUE,
+             cluster_cols=FALSE, 
+             color = colorRampPalette(rev(brewer.pal(n = 9, name = "RdBu")))(100),
+             annotation_col = coldata_mean,
+             breaks = mybreaks,
+             border_color = "white",
+             cellwidth = 20,        
+             cellheight = 10,
+             fontsize_row = 8,
+             annotation_colors = ann_colors)
 
 #ggsave("DEGsANOVA_SpecDiffer.svg", plot = p, width = 6, height = 10.4 )
 
-```
 
-## B: Run DESeq function for pairwise DE analysis WD vs WW
 
-```{r}
+### B: Run DESeq function for pairwise DE analysis WD vs WD
 
 dds <- DESeq(dds)
 
@@ -402,14 +378,9 @@ summary(resSig_XylemDrought)
 # Table S6
 #write.csv(resSig_XylemDrought, file="resSig_XylemDrought_padj0.05_log2FC1.csv")
 
-#allDEGs <- unique(c(rownames(resSig_CorkDrought),rownames(resSig_PhloemDrought), rownames(resSig_XylemDrought)))
-corkDEGs <- rownames(resSig_CorkDrought)
-# get general matrix with normalized counts for all expressed genes
 
-```
 
-### Get VENN diagram for DEGs between WD vs WW determined for each layer
-```{r}
+## Get VENN diagram for DEGs between WD vs WW determined for each layer
 source("bin/GOVenn_corrected.R")
 
 resSig_cork_DvsC <- data.frame(genes=rownames(resSig_CorkDrought), logFC = resSig_CorkDrought[,2])
@@ -418,12 +389,12 @@ resSig_phloem_DvsC <- data.frame(genes=rownames(resSig_PhloemDrought), logFC = r
 
 
 venn<-GOVenn(data1 = resSig_xylem_DvsC, 
-       data2 = resSig_phloem_DvsC, 
-       data3 = resSig_cork_DvsC , 
-       plot=TRUE,
-       title = "Drought DEGs", 
-       label = c("Xylem",  "InnerBark","Phellem" ) , 
-       circle.col = c("#66CCFF","#99FF99", "#FFCC99"))
+             data2 = resSig_phloem_DvsC, 
+             data3 = resSig_cork_DvsC , 
+             plot=TRUE,
+             title = "Drought DEGs", 
+             label = c("Xylem",  "InnerBark","Phellem" ) , 
+             circle.col = c("#66CCFF","#99FF99", "#FFCC99"))
 
 
 #ggsave(plot = venn, file = "Drought_VennDEGs.svg", width = 8.2, height = 8, units = "in", device = "svg")
@@ -431,10 +402,9 @@ venn<-GOVenn(data1 = resSig_xylem_DvsC,
 #ggsave("Drought_VennDEGs.svg", plot = venn, width = 6.2, height = 5.8,device = "svg")
 
 venn_data<-GOVenn(data1 = resSig_phloem_DvsC, 
-       data2 = resSig_xylem_DvsC, 
-       data3 = resSig_cork_DvsC, 
-       plot=FALSE)
+                  data2 = resSig_xylem_DvsC, 
+                  data3 = resSig_cork_DvsC, 
+                  plot=FALSE)
 
 #save.image(file = "DE_workflow.RData")
 #load("DE_workflow.RData")
-```
